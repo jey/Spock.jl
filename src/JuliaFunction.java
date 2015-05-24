44b57ebcb874701d1f87505fc9e562020a92a575
Jey Kottalam <jey@cs.berkeley.edu>
2015-05-23 21:54:45 -0700

    avoid deadlock in worker I/O

--- src/JuliaFunction.java
+++ src/JuliaFunction.java
@@ -1,14 +1,15 @@
 package edu.berkeley.bids.spock;
-import org.apache.spark.rdd.RDD;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function2;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.FileOutputStream;
 import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class JuliaFunction implements Function2<Integer, Iterator<JuliaObject>, Iterator<JuliaObject>> {
   private static final long serialVersionUID = 1;
@@ -29,6 +30,21 @@ public class JuliaFunction implements Function2<Integer, Iterator<JuliaObject>, 
     pb.redirectError(ProcessBuilder.Redirect.INHERIT);
     Process worker = pb.start();
 
+    // start reader
+    final DataInputStream in = new DataInputStream(new BufferedInputStream(worker.getInputStream()));
+    Future<List<JuliaObject>> results =
+      Executors.newSingleThreadExecutor().submit(new Callable<List<JuliaObject>>() {
+        public List<JuliaObject> call() throws Exception {
+          List<JuliaObject> results = new ArrayList<JuliaObject>();
+          while(true) {
+            JuliaObject obj = JuliaObject.read(in);
+            if(obj == null) break;
+            results.add(obj);
+          }
+          return results;
+        }
+      });
+
     // send input
     DataOutputStream out = new DataOutputStream(new BufferedOutputStream(worker.getOutputStream()));
     func.write(out);
@@ -39,20 +55,7 @@ public class JuliaFunction implements Function2<Integer, Iterator<JuliaObject>, 
     out.writeInt(0);
     out.close();
 
-    // read results
-    DataInputStream in = new DataInputStream(new BufferedInputStream(worker.getInputStream()));
-    LinkedList<JuliaObject> results = new LinkedList<JuliaObject>();
-    while(true) {
-      JuliaObject obj = JuliaObject.read(in);
-      if(obj == null) break;
-      results.add(obj);
-    }
-
     // finish up
-    if(worker.waitFor() != 0) {
-      throw new RuntimeException(String.format("Spock worker died with exitValue=%d", worker.exitValue()));
-    } else {
-      return results.iterator();
-    }
+    return results.get().iterator();
   }
 }
